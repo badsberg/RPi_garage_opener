332a1bea12d7fd77b47709dd0e517de0f2c328e6
badsberg <martin@badsberg.net>
2016-08-17 20:42:38 +0200

    Update my_blynk.js

--- my_blynk.js
+++ my_blynk.js
@@ -10,19 +10,36 @@ var blynk = new Blynk.Blynk(process.argv[2], options = {
 });
 
 var GPIO = require('onoff').Gpio,
-    port_switch = new GPIO(7, 'in','both');
+    port_switch_a = new GPIO(7, 'in','both'),
+    port_switch_b = new GPIO(8, 'in','both'),
+    relay_a = new GPIO(11, 'out'),
+    relay_b = new GPIO(9, 'out');
 
-var v9 = new blynk.VirtualPin(9);
+var v1 = new blynk.VirtualPin(1);
+var v2 = new blynk.VirtualPin(2);
+var v3 = new blynk.VirtualPin(3);
+var v4 = new blynk.VirtualPin(4);
 
-port_switch.watch(function(err, state) {
-
+port_switch_a.watch(function(err, state) {
   // check the state of the button
   // 1 == pressed, 0 == not pressed
   if(state == 1) {
     // turn LED on
-    v9.write('1');
+    v1.write('1');
   } else {
     // turn LED off
-    v9.write('0');
+    v1.write('0');
+  }
+});
+
+port_switch_b.watch(function(err, state) {
+  // check the state of the button
+  // 1 == pressed, 0 == not pressed
+  if(state == 1) {
+    // turn LED on
+    v2.write('1');
+  } else {
+    // turn LED off
+    v2.write('0');
   }
 });
